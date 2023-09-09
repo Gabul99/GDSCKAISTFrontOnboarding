8881cefa58f4b3a5e4519e2321f1a57f887a5905
Gabul99 <hclee99@kaist.ac.kr>
2023-09-09 23:04:42 +0900

    HospitalList 파일 미리 생성

--- src/page/router.jsx
+++ src/page/router.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-import { MainLayout, RootLayout } from "./layout/index";
+import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { MainLayout, RootLayout } from './layout/index';
+import HospitalList from '../template/HospitalList';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route path="/main" element={<MainLayout />} />
-    </Route>
-  )
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/main" element={<HospitalList />} />
+      </Route>
+    </Route>,
+  ),
 );
 
 export default router;
---
+++ src/template/HospitalList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HospitalList = () => {
+  return <>앞으로 개발하게 될 곳</>;
+};
+
+export default HospitalList;
