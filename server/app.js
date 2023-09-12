bfb4541c900638774433738e89869b045ae13dea
Gabul99 <hclee99@kaist.ac.kr>
2023-09-12 23:44:24 +0900

    Server 코드 완료

--- server/app.js
+++ server/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send({
-    userName: '이희찬',
-    userNickname: 'Gabul',
-  });
+const registers = [];
+
+app.get('/registers', (req, res) => {
+  res.send(registers);
+});
+
+app.post('/registers', (req, res) => {
+  const randNum = Math.random();
+  if (randNum < 0.1) {
+    res.status(500).json({ status: 500, message: 'UNEXPECTED_ERROR' });
+    return;
+  }
+  const newRegister = req.body;
+  registers.push(newRegister);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log('서버 성공적 실행'));
