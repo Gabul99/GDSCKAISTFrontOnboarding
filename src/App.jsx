3b2ecb6e708af678c9c813c45eb712dd14628e92
Gabul99 <hclee99@kaist.ac.kr>
2023-09-18 20:45:15 +0900

    Recoil 설치 및 Root 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './page/router';
+import { RecoilRoot } from 'recoil';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
 }
 
 export default App;
